12ed21ca12c5f69f3dd2a293817ae17d19167769
Shady-Waheed <shekohitler@gmail.com>
2025-12-24 01:43:19 +0200

    responsive footer

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -19,17 +19,18 @@ export const Newsletter = ({ onValidate, status, message }) => {
   const clearFields = () => {
     setEmail("");
   };
+  // Newsletter.js
   return (
-    <Col lg={12}>
+    <Col xs={12}>
       <div className="newsletter-bx">
-        <Row>
-          <Col lg={12} md={6} xl={5}>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={5}>
             <h3>Subscribe to our Newsletter</h3>
             {status === "sending" && <Alert>Sending...</Alert>}
             {status === "error" && <Alert variant="danger">{message}</Alert>}
             {status === "success" && <Alert variant="success">{message}</Alert>}
           </Col>
-          <Col md={6} xl={7}>
+          <Col xs={12} md={6} xl={7}>
             <form onSubmit={handleSubmit}>
               <div className="new-email-bx">
                 <input
